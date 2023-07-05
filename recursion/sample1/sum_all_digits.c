d82b59065dd9142b1399a4b6d41834a8b4eaf643
VickArmand <victormaina1962@gmail.com>
2023-07-05 13:30:31 +0300

    new func

--- recursion/sample1/sum_all_digits.c
+++ recursion/sample1/sum_all_digits.c
@@ -17,4 +17,7 @@ int main (void)
     printf("%d\n",sum_all_digits(237));
     printf("%d\n",sum_all_digits(230));
     printf("%d\n",sum_all_digits(100));
+    printf("%d\n",sum_all_digits(5));
+    printf("%d\n",sum_all_digits(10));
+    printf("%d\n",sum_all_digits(53));
 }

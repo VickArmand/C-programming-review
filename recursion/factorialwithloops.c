e7e3e4fcc21d8df26477e869f09659a9b4df18cc
VickArmand <victormaina1962@gmail.com>
2023-07-05 12:16:40 +0300

    iterative recursion

---
+++ recursion/factorialwithloops.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *  demonstrating factorial without using recursion
+*/
+
+int factorial (int n)
+{
+    int i, result = 1;
+
+    if (n == 1)
+        return (1);
+    else if (n == 0)
+        return (0);
+    else
+    {
+        i = 1;
+        while(i <= n)
+        {
+            result *= i;
+            i++;
+        }
+        return (result);
+    }
+}
+
+int main(void)
+{
+    printf("%d\n",factorial(0));
+    printf("%d\n",factorial(2));
+    printf("%d\n",factorial(5));
+}
